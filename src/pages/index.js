7b4320a2114d5dfd01c3941403dc16e42deedc93
Ujjal Kar <ujjalkar17@gmail.com>
2019-09-13 20:40:31 +0530

    x

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,59 @@
 import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
+// import { Link, useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
+// import SEO from "../components/seo"
+// import PostCard from "../components/PostCard/PostCard"
 
-// need to fix category page template
-
-import SEO from "../components/seo"
-import PostCard from "../components/PostCard/PostCard"
-const IndexPage = () => <h1>Hell</h1>
+const IndexPage = () => {
+  // const { allMarkdownRemark } = useStaticQuery(graphql`
+  //   query HomePagePost {
+  //     allMarkdownRemark(
+  //       limit: 6
+  //       sort: { fields: frontmatter___date, order: DESC }
+  //     ) {
+  //       node {
+  //         excerpt(pruneLength: 30)
+  //         frontmatter {
+  //           categories
+  //           title
+  //           tags
+  //           date(formatString: "MMM Do YY")
+  //           templateKey
+  //         }
+  //         fields {
+  //           slug
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+  return (
+    <Layout>
+      <div className="col-md-9"></div>
+      <div className="col-md-3">
+        <div className="card">
+          <div className="card-body">
+            <form className="form">
+              <div className="form-group mb-2">
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email adress"
+                />
+              </div>
+              <button
+                className="btn btn-primary d-block w-100 mb-2"
+                type="submit"
+              >
+                I want daily update!
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
 /*
 const IndexPage = () => {
   const { allMarkdownRemark } = useStaticQuery(graphql`
